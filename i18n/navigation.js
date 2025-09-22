847c9553ef75b536a072c026b724647df134e3c3
jessica.giraud <jessica.giraud1@gmail.com>
2025-09-22 16:17:37 +0200

    clean admin part

--- i18n/navigation.js
+++ i18n/navigation.js
@@ -5,6 +5,3 @@ import { routing } from './routing';
 // APIs that consider the routing configuration
 export const { Link, redirect, usePathname, useRouter, getPathname } =
   createNavigation(routing);
-
-export const privatePages = ['/admin(?:/[^/]+)?'];
-export const authPages = ['/login'];
--- middleware.js
+++ middleware.js
@@ -1,41 +1,11 @@
 import createIntlMiddleware from 'next-intl/middleware';
-import { authPages, privatePages } from './i18n/navigation';
-import { routing, locales } from './i18n/routing';
-import { NextResponse } from 'next/server';
-import { auth } from './lib/auth';
-
+import { routing } from './i18n/routing';
 
 const intlMiddleware = createIntlMiddleware(routing);
 
-const testPathnameRegex = (pages, pathName) => {
-  return RegExp(
-    `^(/(${locales.join('|')}))?(${pages
-      .flatMap((p) => (p === '/' ? ['', '/'] : p))
-      .join('|')})/?$`,
-    'i'
-  ).test(pathName);
-};
-
 const middleware = (req) => {
-  const isPrivatePage = testPathnameRegex(privatePages, req.nextUrl.pathname);
-  const isAuthPage = testPathnameRegex(authPages, req.nextUrl.pathname);
-
-  const token = req.cookies.get('token')?.value;
-
-  // Si utilisateur connecté essaie d'aller sur page login/signup
-  if (isAuthPage && token) {
-    return NextResponse.redirect(new URL('/admin/dashboard', req.url));
-  }
-
-  // Si utilisateur non connecté essaie d'aller sur page privée
-  if (isPrivatePage && !token) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
-
   return intlMiddleware(req);
 };
-
-
 
 export const config = {
   // Match all pathnames except for
