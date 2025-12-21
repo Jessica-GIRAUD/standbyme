adeb964599786d587682aa914b2d1958323111f0
Jessica-GIRAUD <jessica.giraud@strateg.in>
2025-12-21 16:51:39 +0100

    fix fullscreen mobile

--- components/SliderVideo.jsx
+++ components/SliderVideo.jsx
@@ -65,7 +65,7 @@ export default function SliderVideo() {
                       height="400"
                       controls
                       preload="metadata"
-                      autoPlay
+                      playsInline
                     />
                   )}
                 </div>
